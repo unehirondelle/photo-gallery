53b9ba5c926adc1459da6daf3cfe04591911a596
Irina Plaksina <irene.plaksina@gmail.com>
2020-10-07 13:24:20 -0400

    Updates

---
+++ precache-manifest.8c0dbb7be87ad5bddc683c259fa00b4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce19b2d4e89512d6eec6a9ab98f568b",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "766f114a3ecdf5136eeb",
+    "url": "/photo-gallery/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e0d728f2daaf01258d3",
+    "url": "/photo-gallery/static/js/2.3d6c58aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photo-gallery/static/js/2.3d6c58aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766f114a3ecdf5136eeb",
+    "url": "/photo-gallery/static/js/main.53236132.chunk.js"
+  },
+  {
+    "revision": "dea700b16e354df5ea7c",
+    "url": "/photo-gallery/static/js/runtime-main.2fb2e379.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photo-gallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
