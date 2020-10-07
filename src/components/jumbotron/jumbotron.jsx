f533059650d37215c5d77cdfea2eaaa4ffc0d392
Irina Plaksina <irene.plaksina@gmail.com>
2020-10-07 17:35:25 -0400

    project cleaned up; jumbotron added

---
+++ src/components/jumbotron/jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './jumbotron.css';
+
+const Jumbotron = () => {
+    return (
+        <header>
+            <h1>Photo Gallery</h1>
+        </header>
+    );
+}
+
+export default Jumbotron;
