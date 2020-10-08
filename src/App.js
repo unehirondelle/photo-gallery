7f9f2a688d07f62533b48b4e5d9e6658f388fa49
Irina Plaksina <irene.plaksina@gmail.com>
2020-10-08 09:56:56 -0400

    basic styles added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Jumbotron from './components/jumbotron/jumbotron';
 import AuthorsList from './components/authorsList/authorsList';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+    let [authors, setAuthors] = useState([]);
+    let [albums, setAlbums] = useState([]);
+    let [pictures, setPictures] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+            const authors = res.data.response;
+            setAuthors(authors);
+        });
+    }, []);
     return (
         <>
             <Jumbotron/>
             <main>
-                <AuthorsList/>
+                <AuthorsList
+                    authors={authors}/>
             </main>
+            <footer>
+                <small>Irina Plaksina &copy; 2020</small>
+            </footer>
         </>
     );
 }
-
-export default App;
