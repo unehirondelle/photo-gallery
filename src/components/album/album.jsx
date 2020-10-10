bf5d63da24e180d7e67a8bea2d1d29352e50c6fa
Irina Plaksina <irene.plaksina@gmail.com>
2020-10-10 04:34:12 -0400

    get pictures list, built pop up on click

--- src/components/album/album.jsx
+++ src/components/album/album.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './album.css';
 import Carousel from "../carousel/carousel";
 
 export default function Album() {
+    const [pictures, setPictures] = useState([]);
+    const [show, setShow] = useState(false);
+    const [currentPicture, setCurrentPicture] = useState(0);
+    useEffect(() => {
+        async function getPictures() {
+            const res = await fetch('https://jsonplaceholder.typicode.com/photos');
+            const result = res.json();
+            return result;
+        }
+
+        getPictures().then(res => {
+            const picturesFromData = res.filter(item => item.albumId === 1);
+            console.log("pictures", picturesFromData);
+            setPictures(picturesFromData);
+        });
+    }, []);
+
     return (
-        <Carousel/>
+        <>
+            <div>
+                {pictures.map(item => (
+                    <button id={item.id} className='thumbnail' onClick={(e) => targetPicture(e)
+                    }>
+                        <img src={item.thumbnailUrl} id={item.id} alt={item.id}/>
+                    </button>)
+                )}
+            </div>
+            <Carousel pictures={pictures} show={show} setShow={setShow} currentPicture={currentPicture}/>
+        </>
     );
+
+    function targetPicture(event) {
+        const currentPicture = event.target.id;
+        setShow(true);
+        setCurrentPicture(currentPicture);
+        console.log('currentPicture', currentPicture);
+        return currentPicture;
+    }
 }
