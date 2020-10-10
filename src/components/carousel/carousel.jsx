ba02a990f50adab37f0b0cfab19a0481a2d4f2ef
Irina Plaksina <irene.plaksina@gmail.com>
2020-10-10 04:40:45 -0400

    added pop up

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -11,33 +11,30 @@ export default function Carousel({pictures, show, setShow, currentPicture}) {
     }, [numCurrentPicture]);
 
     let content = show && (
-            <>
+        <>
+            <div className="slideshow-container">
+                {
+                    pictures.filter(picture => picture.id === currentIndex).map(
+                        picture =>
+                            <div id={picture.id} className="current-slide">
+                                <div className="number">{picture.id} / {pictures.length}</div>
+                                <button type='button' className='close' onClick={() => setShow(false)}>X</button>
+                                <button type='button' className="prev" onClick={previousPicture}>&#10094;</button>
+                                <button type='button' className="next" onClick={nextPicture}>&#10095;</button>
+                                <img src={picture.url} alt="{picture.id}"/>
+                                <div className="caption">{picture.title}</div>
+                            </div>
+                    )
+                }
+            </div>
+        </>
+    );
 
-                <div className="slideshow-container">
-
-                    {
-                        pictures.filter(picture => picture.id === currentIndex).map(
-                            picture =>
-                                <div id={picture.id} className="current-slide">
-                                    <div className="number">{picture.id} / {pictures.length}</div>
-                                    <button type='button' className='close' onClick={() => setShow(false)}>X</button>
-                                    <button type='button' className="prev" onClick={previousPicture}>&#10094;</button>
-                                    <button type='button' className="next" onClick={nextPicture}>&#10095;</button>
-                                    <img src={picture.url} alt="{picture.id}"/>
-                                    <div className="caption">{picture.title}</div>
-                                </div>
-                        )
-                    }
-                </div>
-            </>
-        )
-    ;
     return content
 
     function nextPicture() {
         currentIndex++;
         if (currentIndex > pictures.length) {
-
             setCurrentIndex(currentIndex = 1);
         }
         setCurrentIndex(currentIndex);
