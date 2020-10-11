483efec06e74ad1836987b07b8d80313dfb5da57
Irina Plaksina <irene.plaksina@gmail.com>
2020-10-11 00:39:07 -0400

    got authors & albums lists

--- src/components/authorAlbumsList/authorAlbumsList.jsx
+++ src/components/authorAlbumsList/authorAlbumsList.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './authorAlbumsList.css';
 import Album from "../album/album";
+import album from "../album/album";
 
-export default function AuthorAlbumsList() {
-    return (
-        <Album/>
+export default function AuthorAlbumsList({authors, show, setShow, currentAuthor}) {
+
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        async function getAlbums() {
+            const res = await fetch('https://jsonplaceholder.typicode.com/albums');
+            const result = res.json();
+            return result;
+        }
+
+        getAlbums().then(res => {
+            const albumsFromData = res;
+            console.log("authors", albumsFromData);
+            setAlbums(albumsFromData);
+        });
+    }, []);
+
+    return show && (
+        <>
+            {
+                albums.filter(item => item.userId === +currentAuthor).map(albumUser => (
+
+                    <h1>
+                        {albumUser.title}
+                    </h1>
+                ))
+            }
+            <Album/>
+        </>
     );
 }
--- src/components/authorsList/authorsList.jsx
+++ src/components/authorsList/authorsList.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './authorsList.css';
 import AuthorAlbumsList from "../authorAlbumsList/authorAlbumsList";
+import Carousel from "../carousel/carousel";
 
 export default function AuthorsList() {
+
+    const [authors, setAuthors] = useState([]);
+    const [show, setShow] = useState(false);
+    const [currentAuthor, setCurrentAuthor] = useState(0);
+
+    useEffect(() => {
+        async function getAuthors() {
+            const res = await fetch('https://jsonplaceholder.typicode.com/users');
+            const result = res.json();
+            return result;
+        }
+
+        getAuthors().then(res => {
+            const authorsFromData = res;
+            console.log("authors", authorsFromData);
+            setAuthors(authorsFromData);
+        });
+    }, []);
+
     return (
-        <AuthorAlbumsList/>
+        <>
+            <ul>
+                {authors.map(author => (
+                        <>
+                            <li id={author.id} onClick={(e) => targetAuthor(e)
+                            }>
+                                {author.name}
+                            </li>
+                            <AuthorAlbumsList key={author.id} authors={authors} show={show} setShow={setShow}
+                                              currentAuthor={currentAuthor}/>
+                        </>
+                    )
+                )}
+            </ul>
+
+        </>
     );
+
+    function targetAuthor(event) {
+        const currentAuthor = event.target.id;
+        setShow(true);
+        setCurrentAuthor(currentAuthor);
+        return currentAuthor;
+    }
 }
